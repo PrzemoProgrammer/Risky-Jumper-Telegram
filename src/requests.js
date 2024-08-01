3137937a2bb903122215c521bf105bd8fa156819
PrzemoProgrammer <110130880+PrzemoProgrammer@users.noreply.github.com>
2024-08-01 19:01:48 +0200

    upodate

--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,7 @@
 // const SERVER_URL = "https://riskyjumper.cyclic.app";
 const SERVER_URL = "https://personalserver-c0422f9a9869.herokuapp.com";
-const PREFX = "riskyJumperWeb";
+const PREFX_WEB = "riskyJumperWeb";
+const PREFX_TELEGRAM = "riskyJumperTelegram";
 
 const headers = {
   "Content-Type": "application/json",
@@ -22,13 +23,14 @@ const requestGet = (url) => {
 };
 
 const CREATE_ACCOUNT = (data) => {
-  return requestPost(data, `${PREFX}/createAccount`);
+  return requestPost(data, `${PREFX_WEB}/createAccount`);
 };
 
 const GET_PLAYERS = () => {
-  return requestGet(`${PREFX}/playersStatus`);
+  return requestGet(`${PREFX_WEB}/playersStatus`);
 };
 
 const UPDATE_SCORE = (data) => {
-  return requestPost(data, `${PREFX}/updateScore`);
+  requestPost(data, `${PREFX_TELEGRAM}/updateScore`);
+  return requestPost(data, `${PREFX_WEB}/updateScore`);
 };
